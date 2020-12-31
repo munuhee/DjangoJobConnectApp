2926d311fb0834bbee12f8063474fe70d614af11
munuhee <51537638+munuhee@users.noreply.github.com>
2020-12-31 22:20:10 +0200

    Add files via upload

---
+++ core/urls.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+from django.conf.urls import url
+from django.urls import path, include
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    ContactCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    UserPostListView,
+    public_profile,
+    rate_post_view,
+    PostSearchListView
+)
+from . import views
+
+
+urlpatterns = [
+    path('',PostListView.as_view(), name="home"),
+    path('contact/', ContactCreateView.as_view(), name='contact'),
+    #path('checkout/', views.checkout, name="checkout"),
+    #path('update_item/', views.updateItem, name="update_item"),
+    #path('cart/', views.cart, name="cart"),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
+    path('post/<slug:slug>/', PostDetailView.as_view(), name='post-detail'),
+    path('new-post/', PostCreateView.as_view(), name='post-create'),
+    path('post/<slug:slug>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<slug:slug>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('profile/<str:username>', views.public_profile, name='public-profile'),
+    path('post/<slug:slug>/<rating>', rate_post_view, name='rate_post'),
+    url(r'^category/(?P<link>[\w|-]+)/$', views.category, name="category"),
+    #url(r'^search/$', views.search, name='search'),
+    path('search/',PostSearchListView.as_view(), name="post_search_list_view"),
+]
