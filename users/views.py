00da1d48dd626e16b68696069d731c43ba35bdd0
munuhee <51537638+munuhee@users.noreply.github.com>
2020-12-31 18:44:41 +0200

    Add files via upload

---
+++ users/views.py
@@ -0,0 +1,95 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from django.contrib.auth.models import User
+from rest_framework.views import APIView
+from rest_framework import authentication, permissions
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
+from .models import Profile
+from django.shortcuts import render, redirect, get_object_or_404
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created! You are now able to log in')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        
+        'u_form': u_form,
+        'p_form': p_form
+    }
+
+    return render(request, 'users/profile.html', context)
+
+# This view creates a REST API. Everytime the REST API is accessed through a
+# jQuery button, the authenticated user is added/removed from the list of users
+# who have followed the specific user.
+class FollowUser(APIView):
+    authentication_classes = (authentication.SessionAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get(self, request, format=None, user=None, username=None):
+        obj = get_object_or_404(User, username=username)
+        prof_obj = get_object_or_404(Profile, user=obj)
+        authenticated_user = self.request.user
+        updated = False
+        followed = False
+        if authenticated_user.is_authenticated:
+            if authenticated_user in prof_obj.followers.all():
+                followed = False
+                prof_obj.followers.remove(authenticated_user)
+                follower_count = prof_obj.followers.count()
+                button = 'Follow'
+            else:
+                followed = True
+                prof_obj.followers.add(authenticated_user)
+                follower_count = prof_obj.followers.count()
+                button = 'Unfollow'
+            updated = True
+        data = {
+            "updated": updated,
+            "followed": followed,
+            "follower_count": follower_count,
+            "button": button
+        }
+        return Response(data)
+
+
+class ViewFollowers(LoginRequiredMixin, ListView):
+    model = Profile
+    template_name = 'users/user_followers.html'
+    context_object_name ='profile'
+    ordering = ['-date_posted']
+
+    def get_queryset(self):
+        obj = get_object_or_404(User, username=self.kwargs.get('username'))
+        prof_obj = get_object_or_404(Profile, user=obj)
+        return prof_obj
