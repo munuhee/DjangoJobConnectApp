07efea38e0eb5aa21e8ce4e6c5424a6997a223bb
munuhee <51537638+munuhee@users.noreply.github.com>
2021-01-06 12:41:43 +0200

    Update settings.py

--- murste/settings.py
+++ murste/settings.py
@@ -103,22 +103,15 @@ WSGI_APPLICATION = 'murste.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'MursteFreelance',
+        'NAME': 'murste_db',
         'USER': 'postgres',
-        'PASSWORD': 'testing321',
-        'HOST': '127.0.0.1',
+        'PASSWORD': '2021newresol',
+        'HOST': 'database-1.cwo78vptbdzt.us-east-2.rds.amazonaws.com',
         'PORT': '5432',
-    }
-}
-"""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
