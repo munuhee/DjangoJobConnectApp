04542a0fc4ebc0cccc5d32f8e41510cbf7d9bdeb
munuhee <51537638+munuhee@users.noreply.github.com>
2021-01-07 18:33:20 +0200

    Update settings.py

--- murste/settings.py
+++ murste/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = 'f3v+0c%=5uzb(%)2(c#k^ov+6$h@a+b9mu4sw8^fmqdm-%$0c@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = False
+DEBUG = True
 
 CSRF_COOKIE_SECURE = True
 
