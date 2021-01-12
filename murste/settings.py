1bc14438c3056c9cc4c8f2305f5377339a0913ae
munuhee <51537638+munuhee@users.noreply.github.com>
2021-01-12 14:08:43 +0200

    Update settings.py

--- murste/settings.py
+++ murste/settings.py
@@ -252,7 +252,7 @@ AWS_STORAGE_BUCKET_NAME = 'murste'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 AWS_S3_REGION_NAME="us-east-2"
 AWS_S3_HOST = "s3.us-east-2.amazonaws.com"
 
