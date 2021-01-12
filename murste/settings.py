8a4bac932b3a7cfdb992fdc3430e87fe98d3f691
munuhee <51537638+munuhee@users.noreply.github.com>
2021-01-12 15:54:23 +0200

    Update settings.py

--- murste/settings.py
+++ murste/settings.py
@@ -15,38 +15,6 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['www.murstefreelance.com']
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-            'datefmt' : "%d/%b/%Y %H:%M:%S"
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        },
-    },
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': 'mysite.log',
-            'formatter': 'verbose'
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers':['file'],
-            'propagate': True,
-            'level':'DEBUG',
-        },
-        'MYAPP': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-        },
-    }
-}
 # Application definition
 
 INSTALLED_APPS = [
