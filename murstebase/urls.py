e1c89330bf42409bc79cf4f50501174389c9c518
munuhee <51537638+munuhee@users.noreply.github.com>
2021-01-16 19:58:35 +0300

    req16

--- murstebase/urls.py
+++ murstebase/urls.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from django.conf.urls import url
 from django.urls import path, include
 from . import views
+from django.views.generic.base import TemplateView
 
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('sitemaps/', views.sitemaps, name='sitemaps'),
+    path(
+        "/.well-known/pki-validation/",
+        TemplateView.as_view(template_name="core/8D9F2186D3C951419FEF6C64A2577C8B.txt"),
+    ),
 ]
