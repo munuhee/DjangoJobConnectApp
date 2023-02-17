ea2c02ccc3e72c5a8064620bca5780f4c0ac4479
munuhee <murichustephen@gmail.com>
2023-02-17 14:06:06 +0300

    modified packages

--- murste/settings.py
+++ murste/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = 'f3v+0c%=5uzb(%)2(c#k^ov+6$h@a+b9mu4sw8^fmqdm-%$0c@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['www.Jobspeedyup.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['jobspeedyup.herokuapp.com', 'localhost', '127.0.0.1']
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Application definition
 
